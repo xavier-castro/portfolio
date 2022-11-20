b92b73e97a7b10bfe05fadba59da110687513080
xavier-castro <xavier_castro@icloud.com>
2022-11-20 14:47:38 -0600

    feat(css): setup css in body

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'xaviercastro.me'
+export const SITE_DESCRIPTION =
+  'Portfolio, blog, and random things I care about.'
