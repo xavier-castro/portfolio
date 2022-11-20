28ca11e5c62416d70b803ed1a942425cd1a2cf2e
xavier-castro <xavier_castro@icloud.com>
2022-11-19 18:08:38 -0600

    feat: first commit
    Messed up remotes so had to reset from scratch :)

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+// https://astro.build/config
+import react from '@astrojs/react'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [sitemap(), react()]
+})
