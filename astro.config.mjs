3c242e393eed17f58f8ef8043b609c0a59b93367
xavier-castro <xavier_castro@icloud.com>
2022-11-19 18:15:45 -0600

    feat: add image library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
-import react from '@astrojs/react'
+import react from '@astrojs/react';
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), react()]
-})
+  integrations: [sitemap(), react(), image()]
+});
