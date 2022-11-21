0735ae85e42464d18a530574fa5fcc3e90393322
xavier-castro <xavier_castro@icloud.com>
2022-11-21 00:46:13 -0600

    feat(themetoggle): create themetoggle button

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+const themes = ['light', 'dark']
+
+export default function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  })
+  const toggleTheme = () => {
+    const t = theme === 'light' ? 'dark' : 'light'
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'dark') {
+      root.classList.remove('light')
+    } else {
+      root.classList.add('dark')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted ? <div className="inline-flex">Button</div> : <div />
+}
