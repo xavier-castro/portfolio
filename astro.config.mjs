ae9778726180d13a51672671c9fd9ff710df9f62
xavier-castro <xavier_castro@icloud.com>
2022-11-23 13:58:31 -0600

    chore(clean): start on fresh commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-// https://astro.build/config
-import react from '@astrojs/react';
-
-// https://astro.build/config
-import image from '@astrojs/image';
-
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xaviercastro.dev',
   integrations: [sitemap(), react(), image(), tailwind()]
-});
+})
