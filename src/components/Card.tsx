93731977d9b9f53a5ac11a045570b8ac500edee2
xavier-castro <xavier_castro@icloud.com>
2022-12-02 12:57:40 -0600

    feat(playground): have playground and card ready to be fully testable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,13 @@ type CardProps = {
 }
 
 const Card: React.FC<CardProps> = ({ title }) => {
-  return <>{title}</>
+  return (
+    <>
+      <div className="flex justify-center items-center bg-pink-500 h-80 mb-6 rounded-lg">
+        {title}
+      </div>
+    </>
+  )
 }
 
 export default Card
