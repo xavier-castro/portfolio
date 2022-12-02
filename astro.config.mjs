1c0cdbe40beaca535d79312f574153adeed6e996
xavier-castro <xavier_castro@icloud.com>
2022-12-01 22:55:00 -0600

    feat(astro.config): add rehype

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,16 @@ export default defineConfig({
     react(),
     image({ serviceEntryPoint: '@astrojs/image/sharp' }),
     tailwind()
-  ]
+  ],
+  markdown: {
+    extendedDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl'
+        }
+      ]
+    ]
+  }
 })
