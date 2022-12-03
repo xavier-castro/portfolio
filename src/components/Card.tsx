a4a04d17b7c0fc889586746d98a44eec4972bd8b
xavier-castro <xavier_castro@icloud.com>
2022-12-02 19:52:05 -0600

    feat(listitem): add ListItem component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,18 @@
 type CardProps = {
   title: string
+  info: string
 }
 
-const Card: React.FC<CardProps> = ({ title }) => {
+const Card: React.FC<CardProps> = ({ title, info }) => {
   return (
     <>
-      <div className="flex justify-center items-center bg-pink-500 h-80 mb-6 rounded-lg">
-        {title}
+      <div className="p-16 flex justify-center items-center bg-pink-500 h-80 mb-6 rounded-lg mx-auto flex-col">
+        <div>
+          <h1 className="prose prose-h1 text-6xl mb-4">{title}</h1>
+        </div>
+        <div>
+          <p>{info}</p>
+        </div>
       </div>
     </>
   )
